136c6d57c2352fed81a9d391a1a3bacd78438b8e
floss-order <flossOrder@gmail.com>
2021-03-16 23:37:49 +0000

    Set up the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import { Switch, Route } from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
-    <Home />
-  )
+      <Switch>
+        <PrivateRoute path="/" component={Home} exact />
+        <Route path="/login" component={Login} />
+      </Switch>
+  )  
 }
 
 export default App

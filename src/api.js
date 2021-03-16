337d43eaf228b147924fded2d636d5dc1684097c
floss-order <flossOrder@gmail.com>
2021-03-16 20:58:03 +0000

    Refactored data fetching function

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
 const BASE_URL = 'http://ideadeploy.space/test/'
 
 export async function getGraphData() {
-    const graphData = await getData('graph')
+    const graphData = await fetchData('graph')
     return graphData
 }
 
 export async function getDonutData() {
-    const donutData = await getData('donut')
+    const donutData = await fetchData('donut')
     return donutData
 }
 
-async function getData(endpoint) {
-    const request = await fetch(BASE_URL + endpoint + '.json')
+async function fetchData(endpoint, options = undefined) {
+    const request = await fetch(BASE_URL + endpoint + '.json', options)
     const data = await request.json()
     return data
 }
